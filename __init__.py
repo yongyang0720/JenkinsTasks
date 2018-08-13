1bd09e97906847859813c36095de5d004b51a56e
yong yang <yongyang0720@126.com>
2018-08-13 17:29:24 +0800

    Code style

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    App and Jobs Class
+    Task Package
     ~~~~~~~~~~~~
-    App and Jobs Class
+    Task Packages
 
 """
 
---
+++ test/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    TestJobs and TestApp Class
+    ~~~~~~~~~~~~
+    TestJobs and TestApp Class
+
+"""
+
+__all__ = ['flask', 'jenkins', 'configparser', 'sys', 'unittest']
+
+from Tasks import *
